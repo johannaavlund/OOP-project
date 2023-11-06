ebdd4d3cc5d33c956e5c5147fa7edb47153e7ac5
johannaavlund <117651604+johannaavlund@users.noreply.github.com>
2023-11-06 16:05:53 +0100

    Delete pacmanproject/pacmanapp/PacManapp directory

--- pacmanproject/pacmanapp/PacManapp/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.pacmanapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.pacmanapp to javafx.fxml;
-    exports com.example.pacmanapp;
-}
