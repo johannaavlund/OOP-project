3f1a917f9aada52ae799837fc5feda560dc8dac3
hannistuis <hannah.tuu.hannah@gmail.com>
2023-11-08 15:02:02 +0100

    ghost början

--- Pacman/src/Ghost.java
+++
@@ -1,6 +0,0 @@
-import Model.Movable;
-import Model.Positionable;
-
-public abstract class Ghost implements Movable, Positionable {
-    
-}
--- Pacman/src/Model/Ghost.java
+++ Pacman/src/Model/Ghost.java
@@ -1,4 +1,6 @@
+
 package Model;
+
 public abstract class Ghost implements Movable, Positionable {
     private double pos_x;
     private double pos_y;
@@ -6,14 +8,16 @@ public abstract class Ghost implements Movable, Positionable {
     private double speed_x;
     private double speed_y;
     private String name;
+    private int number_of_ghost;
     //state ??
 
-    public Ghost(double pos_x, double pos_y, double speed_x, double speed_y, String name){
+    public Ghost(double pos_x, double pos_y, double speed_x, double speed_y, String name, int number_of_ghost){
         this.pos_x = pos_x;
         this.pos_y = pos_y;
         this.speed_x = speed_x;
         this.speed_y = speed_y;
         this.name = name;
+        this.number_of_ghost = number_of_ghost;
     }
 
     public double getPosX(){
@@ -35,5 +39,34 @@ public abstract class Ghost implements Movable, Positionable {
     public String getGhostName(){
         return this.name;
     }
-    
+
+    public void setPosX(double x){
+        this.pos_x = x;
+    }
+    public void setPosY(double y){
+        this.pos_y = y;
+    }
+    public void setSpeedX(double speedX){
+        this.speed_x = speedX;
+    }
+    public void setSpeedY(double speedY){
+        this.speed_y = speedY;
+    }
+    public void setName(double name){
+        this.name = name;
+    }
+
+    @Override
+    public void move(){
+        int random;
+        int current_pos;
+        int count;
+        for (int i = 0; i < number_of_ghost; i++ ){
+            if (pos_x[i] % BLOCK_SIZE == 0 && pos_y[i] % BLOCK_SIZE == 0) {
+                pos = pos_x[i] / BLOCK_SIZE + N_BLOCKS * (int) (pos_y[i] / BLOCK_SIZE);
+
+                count = 0;
+        }
+
+    }
 }
