c07ae3f23fa0b6b66c330b4a478c87d307804991
johannaavlund <johanna.avlund@hotmail.com>
2023-11-08 14:01:36 +0100

    pacman app

---
+++ Pacman/src/PacmanApp.java
@@ -0,0 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
+import View.DrawWorld;
+
+import java.awt.*;
+
+
+public class PacmanApp {
+
+    private static DrawWorld world;
+ 
+
+    public static void main(String[] args){
+        world = new DrawWorld(800,800);
+        Window window = new Window("first world", world);
+    }
+
+}
